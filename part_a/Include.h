5aab1dbbb037e6f76d028f8ca0afb0c7acd97ce7
mosguinz <mos.guinz@gmail.com>
2022-04-11 02:04:56 +0700

    Add placeholder for LinkedBag functions

--- part_a/Include.h
+++ part_a/Include.h
@@ -1,1 +1,13 @@
-//	Include.h
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <cstdlib>
+
+#include "LinkedBag.h"
+#include "LinkedBag.cpp"
+#include "LinkedBag340.cpp"
+
+using namespace std;
--- part_a/LinkedBag340.cpp
+++ part_a/LinkedBag340.cpp
@@ -1,8 +1,52 @@
-//	LinkedBag340.cpp
-//	Created by: CSC340
+#include "LinkedBag.h"
 
-/*
-===>					<===
-===>  PLEASE ADD CODE	<===
-===>					<===
-*/
+template<typename ItemType>
+bool LinkedBag<ItemType>::removeSecondNode340() {
+    return false;
+}
+
+template<typename ItemType>
+bool LinkedBag<ItemType>::addEnd340(const ItemType &) {
+    return false;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340Iterative() const {
+    return 0;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340Recursive() const {
+    return 0;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340RecursiveHelper(Node<ItemType> *) const {
+    return 0;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getCurrentSize340RecursiveNoHelper() const {
+    return 0;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getFrequencyOf340Recursive(const ItemType &) const {
+    return 0;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getFrequencyOf340RecursiveHelper(Node<ItemType> *, const ItemType &) const {
+    return 0;
+}
+
+template<typename ItemType>
+int LinkedBag<ItemType>::getFrequencyOf340RecursiveNoHelper(const ItemType &) const {
+    return 0;
+}
+
+template<typename ItemType>
+ItemType LinkedBag<ItemType>::removeRandom340() {
+    return nullptr;
+}
+
