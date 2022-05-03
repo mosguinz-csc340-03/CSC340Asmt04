ba23222e4d4ec4b4a34a192d8421a4566041b9b4
mosguinz <mos.guinz@gmail.com>
2022-05-03 08:34:04 +0700

    Reformat output for (b)(1)

--- part_b/main.cpp
+++ part_b/main.cpp
@@ -8,30 +8,31 @@
 void B1() {
 
     std::string *a = new std::string{"Bob"};
-    std::cout << "Created an `std::string` using `new`; the pointer is captured to a variable `a`" << std::endl;
-    std::cout << "Dereferencing `a` yields: " << *a << std::endl;
+    std::cout << "Created an `std::string` using `new`; the pointer is captured to a variable `a`" << std::endl
+              << "Dereferencing `a` yields: " << *a << std::endl << std::endl;
 
     std::string *b = a;
-    std::cout << "Created another pointer `b` to have the same value as `a`" << std::endl;
-    std::cout << "Both pointers `a` and `b` points to " << a << std::endl;
+    std::cout << "Created another pointer `b` to have the same value as `a`" << std::endl
+              << "Both pointers `a` and `b` points to " << a << std::endl << std::endl;
 
     std::cout << "Applying `delete` to `a` will return the string's memory to the free-store" << std::endl;
     delete a;
 
     std::cout << "Since `delete` removes the object and not the pointer, printing `a` and `b` shows "
-                 "that they still point to the same location:" << std::endl;
-    std::cout << "a: " << a << "  |  b: " << b << std::endl;
+                 "that they still point to the same location:" << std::endl
+              << "a: " << a << "  |  b: " << b << std::endl << std::endl;
 
     std::cout << "This means that `a` and `b` are now dangling because they both point to deallocated memory"
+              << std::endl
+              << "Attempting to apply `delete` on to `b` will result in an error, for example:" << std::endl
               << std::endl;
-    std::cout << "Attempting to apply `delete` on to `b` will result in an error, for example:" << std::endl;
 
     // delete b;
 
     // In Clang,
     std::cout << "part_b(36739," << a << ") malloc: *** error for object " << a
-              << ": pointer being freed was not allocated" << std::endl;
-    std::cout << "part_b(36739," << a << ") malloc: *** set a breakpoint in malloc_error_break to debug" << std::endl;
+              << ": pointer being freed was not allocated" << std::endl
+              << "part_b(36739," << a << ") malloc: *** set a breakpoint in malloc_error_break to debug" << std::endl;
 
 }
 
