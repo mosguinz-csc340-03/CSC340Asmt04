08894a5ed0520f0040d1ed5b863b9966ef33144c
mosguinz <mos.guinz@gmail.com>
2022-05-04 09:26:59 +0700

    Convert headPtr to smart_ptr

--- PartC_SmartPointers/LinkedBag.h
+++ PartC_SmartPointers/LinkedBag.h
@@ -39,7 +39,8 @@ class LinkedBag : public BagInterface<ItemType> {
     std::vector<ItemType> toVector() const;
 
  private:
-    Node<ItemType> *headPtr{nullptr}; // Pointer to first node
+    std::unique_ptr<Node<ItemType>> headPtr{nullptr};
+//    Node<ItemType> *headPtr{nullptr}; // Pointer to first node
     int itemCount{0};                    // Current count of bag items
 
     // pointer to the node or the null pointer
